5922c4bb8db4e49974c7578f4afc75d5e2518629
pedro vito <pedro.v.oliveira007@gmail.com>
2024-06-11 04:37:54 +0000

    finalizado

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,25 @@
 import { IconContainer, CardContainer, ButtonContainer, ResultPContainer } from "./Home.styled"
 import iconStarImg from "../assets/icon-star.svg"
 import illustration from "../assets/illustration-thank-you.svg"
+import { useState } from "react"
 
 export function Home() {
-  let aparecerCard = true
+  const [mostraResultado, setMostraResultado] = useState(false)
+    const[feedbackNote, setFeedbackNote] = useState(0)
+
+  function handleFeedbackButtonClick(event){
+    const feedback = Number(event.target.innerText )
+
+    setFeedbackNote(feedback)
+  }
+  function handlesubmit (){
+    if (feedbackNote === 0) return
+    setMostraResultado(true)
+  }
+
+  console.log(feedbackNote)
   return (
-    aparecerCard === false ?(
+    mostraResultado === false ?(
 
 
       <CardContainer>
@@ -17,19 +31,19 @@ export function Home() {
       <p>conete-nos como foi nossa ajuda com a solicitação. Agradecemos muito o seu feedback para podemos melhorar nosso atendimento! </p>
 
       <ButtonContainer>
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>4</button>
-        <button>5</button>
+        <button onClick={handleFeedbackButtonClick}>1</button>
+        <button onClick={handleFeedbackButtonClick}>2</button>
+        <button onClick={handleFeedbackButtonClick}>3</button>
+        <button onClick={handleFeedbackButtonClick}>4</button>
+        <button onClick={handleFeedbackButtonClick}>5</button>
       </ButtonContainer>
-      <button>enviar</button>
+      <button onClick={handlesubmit}>enviar</button>
     </CardContainer>
     ): (
       <CardContainer>
         <img src={illustration} alt="imagem de agradecimento" />
         <ResultPContainer>
-          <p>Voce selecionol 4/5</p>
+          <p>Voce selecionol {feedbackNote}/5</p>
         </ResultPContainer>
         <h1>
           Obrigado!
